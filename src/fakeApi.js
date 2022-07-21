2f6246b4adb677fdb79ebd772566c9dc9998b041
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2022-07-21 14:53:25 -0700

    remove fetchPosts()

--- src/fakeApi.js
+++ src/fakeApi.js
@@ -40,26 +40,3 @@ function fetchUser() {
     }, 2000);
   });
 }
-
-function fetchPosts() {
-  console.log("fetch posts...");
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("fetched posts");
-      resolve([
-        {
-          id: 0,
-          text: "I get by with a little help from my friends"
-        },
-        {
-          id: 1,
-          text: "I'd like to be under the sea in an octupus's garden"
-        },
-        {
-          id: 2,
-          text: "You got that sand all over your feet"
-        }
-      ]);
-    }, 4000);
-  });
-}
